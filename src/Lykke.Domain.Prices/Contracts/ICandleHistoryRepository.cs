dbd0381b15b35713812b874bb06e5d0662a587b7
Petr Giloian <giloyanp@gmail.com>
2017-02-21 00:14:46 +0700

    Added method to ICandleHistoryRepository.

--- src/Lykke.Domain.Prices/Contracts/ICandleHistoryRepository.cs
+++ src/Lykke.Domain.Prices/Contracts/ICandleHistoryRepository.cs
@@ -8,6 +8,7 @@ namespace Lykke.Domain.Prices.Contracts
     public interface ICandleHistoryRepository
     {
         Task InsertOrMergeAsync(ICandle feedCandle, string asset, TimeInterval interval);
+        Task InsertOrMergeAsync(IEnumerable<ICandle> candles, string asset, TimeInterval interval);
         Task<ICandle> GetCandleAsync(string asset, TimeInterval interval, bool isBuy, DateTime dateTime);
         //Task<IEnumerable<ICandle>> GetCandlesAsync(string asset, TimeInterval interval, bool isBuy, DateTime dateTime);
         Task<IEnumerable<ICandle>> GetCandlesAsync(string asset, TimeInterval interval, bool isBuy, DateTime from, DateTime to);
