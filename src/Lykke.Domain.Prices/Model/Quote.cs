90e6d35cace26c529148ac1479aeafebc59c7c9b
Petr Giloian <giloyanp@gmail.com>
2017-03-02 11:45:39 +0700

    Removed Equals override.

--- src/Lykke.Domain.Prices/Model/Quote.cs
+++ src/Lykke.Domain.Prices/Model/Quote.cs
@@ -9,18 +9,5 @@ namespace Lykke.Domain.Prices.Model
         public bool IsBuy { get; set; }
         public double Price { get; set; }
         public DateTime Timestamp { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            Quote other = obj as Quote;
-            if (other == null)
-            {
-                return false;
-            }
-            return (string.Equals(this.AssetPair, other.AssetPair, StringComparison.OrdinalIgnoreCase)
-                && this.IsBuy == other.IsBuy
-                && this.Price == other.Price
-                && this.Timestamp == other.Timestamp);
-        }
     }
 }
