966dd428482c9072910df9ed91c652fcaed50deb
Petr Giloian <giloyanp@gmail.com>
2017-03-02 11:24:14 +0700

    Added time intervals.

--- src/Lykke.Domain.Prices/Enums/TimeInterval.cs
+++ src/Lykke.Domain.Prices/Enums/TimeInterval.cs
@@ -11,7 +11,11 @@ namespace Lykke.Domain.Prices
         Min15 = 900,
         Min30 = 1800,
         Hour = 3600,
+        Hour4 = 7200,
+        Hour6 = 21600,
+        Hour12 = 43200,
         Day = 86400,
+        Week = 604800,
         Month = 3000000
     }
 }
