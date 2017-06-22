30e518e579d2bd0fba264312250f93c48be6689d
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-06-22 21:54:05 +0300

    - Candle merging with new custom date - Candles collection merging

--- src/Lykke.Domain.Prices/CandleExtensions.cs
+++ src/Lykke.Domain.Prices/CandleExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Lykke.Domain.Prices.Contracts;
 using Lykke.Domain.Prices.Model;
 
@@ -11,8 +12,13 @@ namespace Lykke.Domain.Prices
         /// </summary>
         /// <param name="prevCandle">Previous candle</param>
         /// <param name="nextCandle">Next candle</param>
+        /// <param name="dateTime">
+        /// <see cref="IFeedCandle.DateTime"/> of merged candle, if not specified, 
+        /// then <see cref="IFeedCandle.DateTime"/> of both candles should be equal, 
+        /// and it will be used as merged candle <see cref="IFeedCandle.DateTime"/>
+        /// </param>
         /// <returns></returns>
-        public static IFeedCandle MergeWith(this IFeedCandle prevCandle, IFeedCandle nextCandle)
+        public static IFeedCandle MergeWith(this IFeedCandle prevCandle, IFeedCandle nextCandle, DateTime? dateTime = null)
         {
             if (prevCandle == null || nextCandle == null)
             {
@@ -23,7 +29,8 @@ namespace Lykke.Domain.Prices
             {
                 throw new InvalidOperationException($"Can't merge buy and sell candles. Source={prevCandle.ToJson()} Update={nextCandle.ToJson()}");
             }
-            if (prevCandle.DateTime != nextCandle.DateTime)
+
+            if (!dateTime.HasValue && prevCandle.DateTime != nextCandle.DateTime)
             {
                 throw new InvalidOperationException($"Can't merge candles with different DateTime property. Source={prevCandle.ToJson()} Update={nextCandle.ToJson()}");
             }
@@ -35,8 +42,85 @@ namespace Lykke.Domain.Prices
                 High = Math.Max(prevCandle.High, nextCandle.High),
                 Low = Math.Min(prevCandle.Low, nextCandle.Low),
                 IsBuy = prevCandle.IsBuy,
-                DateTime = prevCandle.DateTime
+                DateTime = dateTime ?? prevCandle.DateTime
             };
+        }
+
+        /// <summary>
+        /// Merges all of candles placed in chronological order
+        /// </summary>
+        /// <param name="candles">Candles in hronological order</param>
+        /// <param name="dateTime">
+        /// <see cref="IFeedCandle.DateTime"/> of merged candle, if not specified, 
+        /// then <see cref="IFeedCandle.DateTime"/> of all candles should be equal, 
+        /// and it will be used as merged candle <see cref="IFeedCandle.DateTime"/>
+        /// </param>
+        /// <returns>Merged candle, or null, if no candles to merge</returns>
+        public static IFeedCandle MergeAll(this ICollection<IFeedCandle> candles, DateTime? dateTime = null)
+        {
+            if (candles == null)
+            {
+                return null;
+            }
+
+            var open = 0d;
+            var close = 0d;
+            var high = 0d;
+            var low = 0d;
+            var firstCandleIsBuy = false;
+            var firstCandleDateTime = DateTime.MinValue;
+            var count = 0;
+
+            using (var enumerator = candles.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    var candle = enumerator.Current;
+
+                    if (count == 0)
+                    {
+                        open = candle.Open;
+                        close = candle.Close;
+                        high = candle.High;
+                        low = candle.Low;
+                        firstCandleIsBuy = candle.IsBuy;
+                        firstCandleDateTime = candle.DateTime;
+                    }
+                    else
+                    {
+                        if (firstCandleIsBuy != candle.IsBuy)
+                        {
+                            throw new InvalidOperationException($"Can't merge buy and sell candles. Current candle={candle.ToJson()}");
+                        }
+
+                        if (!dateTime.HasValue && firstCandleDateTime != candle.DateTime)
+                        {
+                            throw new InvalidOperationException($"Can't merge candles with different DateTime property. Current candle={candle.ToJson()}");
+                        }
+
+                        close = candle.Close;
+                        high = Math.Max(high, candle.High);
+                        low = Math.Min(low, candle.Low);
+                    }
+
+                    count++;
+                }
+            }
+
+            if (count > 0)
+            {
+                return new FeedCandle
+                {
+                    Open = open,
+                    Close = close,
+                    High = high,
+                    Low = low,
+                    IsBuy = firstCandleIsBuy,
+                    DateTime = dateTime ?? firstCandleDateTime
+                };
+            }
+
+            return null;
         }
 
         public static string ToJson(this IFeedCandle candle)
@@ -45,5 +129,10 @@ namespace Lykke.Domain.Prices
                 ? $"{{ open:{candle.Open} close:{candle.Close} high:{candle.High} low:{candle.Low} isBuy:{candle.IsBuy} dateTime:{candle.DateTime:o} }}"
                 : string.Empty;
         }
+    }
+
+    public class CandlesMerger
+    {
+        
     }
 }
--- test/unit/Lykke.Domain.Prices.Tests/CandleExtensionsTests.cs
+++ test/unit/Lykke.Domain.Prices.Tests/CandleExtensionsTests.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
 using Lykke.Domain.Prices.Contracts;
+using Xunit;
 using Lykke.Domain.Prices.Model;
 
 namespace Lykke.Domain.Prices.Tests
@@ -13,15 +10,16 @@ namespace Lykke.Domain.Prices.Tests
         [Fact]
         public void PriceValuesAreMerged()
         {
-            DateTime dt = new DateTime(2017, 1, 1);
+            var dt = new DateTime(2017, 1, 1);
 
             // Case 1: Update values are taken
             //
             var target = new FeedCandle() { Open = 2, Close = 3, High = 4, Low = 1, IsBuy = true, DateTime = dt };
             var update = new FeedCandle() { Open = 3, Close = 2, High = 5, Low = 0.1, IsBuy = true, DateTime = dt };
 
-            IFeedCandle merged = target.MergeWith(update);
-            Assert.Equal(update.Open, merged.Open);
+            var merged = target.MergeWith(update);
+
+            Assert.Equal(target.Open, merged.Open);
             Assert.Equal(update.Close, merged.Close);
             Assert.Equal(update.High, merged.High);
             Assert.Equal(update.Low, merged.Low);
@@ -33,8 +31,24 @@ namespace Lykke.Domain.Prices.Tests
             update = new FeedCandle() { Open = 3, Close = 2, High = 3, Low = 2, IsBuy = true, DateTime = dt };
 
             merged = target.MergeWith(update);
+
             Assert.Equal(target.High, merged.High);
             Assert.Equal(target.Low, merged.Low);
+        }
+
+        [Fact]
+        public void NewDateIsUsedAndCandlesMayHaveDifferentDates()
+        {
+            // Arrange
+            var newDate = new DateTime(2017, 1, 3);
+            var target = new FeedCandle { Open = 2, Close = 3, High = 4, Low = 1, IsBuy = true, DateTime = new DateTime(2017, 1, 1) };
+            var update = new FeedCandle { Open = 3, Close = 2, High = 5, Low = 0.1, IsBuy = true, DateTime = new DateTime(2017, 1, 2) };
+
+            // Act
+            var merged = target.MergeWith(update, newDate);
+
+            // Assert
+            Assert.Equal(merged.DateTime, newDate);
         }
 
         [Fact]
@@ -54,6 +68,114 @@ namespace Lykke.Domain.Prices.Tests
             var update = new FeedCandle() { Open = 3, Close = 2, High = 5, Low = 0.1, IsBuy = true, DateTime = new DateTime(2017, 1, 2) };
 
             Assert.Throws(typeof(InvalidOperationException), () => target.MergeWith(update));
+        }
+
+        [Fact]
+        public void EmptyCollectionMergedToNull()
+        {
+            // Arrange
+            var candles = new IFeedCandle[0];
+
+            // Act
+            var merged = candles.MergeAll();
+
+            // Assert
+            Assert.Null(merged);
+        }
+
+        [Fact]
+        public void SingleElementCollectionMergedToEqualElement()
+        {
+            // Arrange
+            var candles = new IFeedCandle[]
+            {
+                new FeedCandle {Open = 2, Close = 3, High = 4, Low = 1, IsBuy = true, DateTime = new DateTime(2017, 1, 1)}
+            };
+
+            // Act
+            var merged = candles.MergeAll();
+
+            // Assert
+            Assert.NotNull(merged);
+            Assert.Equal(candles[0].Open, merged.Open);
+            Assert.Equal(candles[0].Close, merged.Close);
+            Assert.Equal(candles[0].High, merged.High);
+            Assert.Equal(candles[0].Low, merged.Low);
+            Assert.Equal(candles[0].IsBuy, merged.IsBuy);
+            Assert.Equal(candles[0].DateTime, merged.DateTime);
+        }
+
+        [Fact]
+        public void CollectionMergedCorrectly()
+        {
+            // Arrange
+            var candles = new IFeedCandle[]
+            {
+                new FeedCandle {Open = 2, Close = 3, High = 3, Low = 2, IsBuy = true, DateTime = new DateTime(2017, 1, 1)},
+                new FeedCandle {Open = 3, Close = 2, High = 4, Low = 3, IsBuy = true, DateTime = new DateTime(2017, 1, 1)},
+                new FeedCandle {Open = 1, Close = 4, High = 2, Low = 1, IsBuy = true, DateTime = new DateTime(2017, 1, 1)},
+                new FeedCandle {Open = 4, Close = 1, High = 5, Low = 4, IsBuy = true, DateTime = new DateTime(2017, 1, 1)}
+            };
+
+            // Act
+            var merged = candles.MergeAll();
+
+            // Assert
+            Assert.NotNull(merged);
+            Assert.Equal(candles[0].Open, merged.Open);
+            Assert.Equal(candles[3].Close, merged.Close);
+            Assert.Equal(candles[3].High, merged.High);
+            Assert.Equal(candles[2].Low, merged.Low);
+            Assert.Equal(candles[0].IsBuy, merged.IsBuy);
+            Assert.Equal(candles[0].DateTime, merged.DateTime);
+        }
+
+        [Fact]
+        public void NewDateIsUsedAndCandlesCollectionMayHaveDifferentDates()
+        {
+            // Arrange
+            var newDate = new DateTime(2017, 1, 5);
+            var candles = new IFeedCandle[]
+            {
+                new FeedCandle {Open = 2, Close = 3, High = 3, Low = 2, IsBuy = true, DateTime = new DateTime(2017, 1, 1)},
+                new FeedCandle {Open = 3, Close = 2, High = 4, Low = 3, IsBuy = true, DateTime = new DateTime(2017, 1, 2)},
+                new FeedCandle {Open = 1, Close = 4, High = 2, Low = 1, IsBuy = true, DateTime = new DateTime(2017, 1, 3)},
+                new FeedCandle {Open = 4, Close = 1, High = 5, Low = 4, IsBuy = true, DateTime = new DateTime(2017, 1, 4)}
+            };
+
+            // Act
+            var merged = candles.MergeAll(newDate);
+
+            // Assert
+            Assert.Equal(merged.DateTime, newDate);
+        }
+
+        [Fact]
+        public void MergingBuySellCollectionIsUnsupported()
+        {
+            // Arrange
+            var candles = new IFeedCandle[]
+            {
+                new FeedCandle {Open = 2, Close = 3, High = 3, Low = 2, IsBuy = true, DateTime = new DateTime(2017, 1, 1)},
+                new FeedCandle {Open = 3, Close = 2, High = 4, Low = 3, IsBuy = false, DateTime = new DateTime(2017, 1, 1)},
+            };
+
+            // Act/Assert
+            Assert.Throws(typeof(InvalidOperationException), () => candles.MergeAll());
+        }
+
+        [Fact]
+        public void MergingCandlesCollectionWithDiffTimeIsUnsupported()
+        {
+            // Arrange
+            var candles = new IFeedCandle[]
+            {
+                new FeedCandle {Open = 2, Close = 3, High = 3, Low = 2, IsBuy = true, DateTime = new DateTime(2017, 1, 1)},
+                new FeedCandle {Open = 3, Close = 2, High = 4, Low = 3, IsBuy = true, DateTime = new DateTime(2017, 1, 2)},
+            };
+
+            // Act/Assert
+            Assert.Throws(typeof(InvalidOperationException), () => candles.MergeAll());
         }
     }
 }
